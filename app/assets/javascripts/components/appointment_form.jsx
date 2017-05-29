d28a5a292532ce7c571fe8978a49b9cc09e33451
F3PiX <f3pixnl@gmail.com>
2017-05-29 19:37:35 +0200

    use state in appointments comp and pass it to form comp

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -4,8 +4,8 @@ var AppointmentForm = React.createClass({
       <div>
         <h3>Add an appointment</h3>
         <form>
-          <input name="event" placeholder="What?"/>
-          <input name="slot" placeholder="When?"/>
+          <input name="event" placeholder="What?" value={this.props.input_event}/>
+          <input name="slot" placeholder="When?" value={this.props.input_slot}/>
           <input type="submit" value="Add to calendar"/>
         </form>
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,8 +1,18 @@
 var Appointments = React.createClass({
+  getInitialState: function () {
+    return {
+      appointments: this.props.appointments,
+      input_event: 'what?',
+      input_slot: 'when...?'
+    }
+  },
+
   render: function () {
     return (
       <div>
-        <AppointmentForm />
+        <AppointmentForm
+          input_event={this.state.input_event}
+          input_slot={this.state.input_slot} />
         <AppointmentsList appointments={this.props.appointments} />
       </div>
     )
